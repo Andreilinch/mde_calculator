9306115c550f3fae9d70ad93e0fcaeb43be04fdd
Andrey <andreikorotkiy@gmail.com>
2021-06-09 00:25:38 +0300

    Initial calculation testing

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html',current_title = 'Custom Title')
+    if request.method == 'POST':
+        df = pd.read_csv(request.files.get('file'))
+        return render_template('index.html',  current_title = 'Custom Title',shape=df.shape)
+    else:
+        return render_template('index.html', current_title = 'Custom Title')
+
+# @app.route('/result', methods=['GET', 'POST'])
+# def result():
+#     if request.method == 'POST':
+#         df = pd.read_csv(request.files.get('file'))
+#         return render_template('result.html',  shape=df.shape)
+#     return render_template('index.html')
+
 @app.route('/about')
 def about():
-    return render_template('about.htmls')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
